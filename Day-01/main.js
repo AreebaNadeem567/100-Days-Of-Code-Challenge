af67ac477f43a722572cce35751e7428c6251ac4
AREEBA NADEEM <AreebaNadeem567>
2024-08-10 22:02:59 +0500

    first commit

---
+++ Day-01/main.js
@@ -0,0 +1,11 @@
+// Q1 - Install Node.js, TypeScript and VS Code on your computer.
+/* Q2 - Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Areeba, would you like to learn some Python today?”*/
+var myName = "Areeba";
+console.log("Hello ".concat(myName, ", would you like to learn some Python today?\u201D"));
+/* Q3 - Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase,
+uppercase, and titlecase.*/
+var personName = "Areeba";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
---
+++ Day-01/main.ts
@@ -0,0 +1,20 @@
+// Q1 - Install Node.js, TypeScript and VS Code on your computer.
+
+/* Q2 - Personal Message: Store a person’s name in a variable, and print a message to that person. 
+Your message should be simple, such as, “Hello Areeba, would you like to learn some Python today?”*/
+
+let myName: string = "Areeba";
+
+console.log(`Hello ${myName}, would you like to learn some Python today?”`);
+
+
+/* Q3 - Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
+uppercase, and titlecase.*/
+
+let personName: string = "Areeba";
+
+console.log(personName.toLowerCase());
+
+console.log(personName.toUpperCase());
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
