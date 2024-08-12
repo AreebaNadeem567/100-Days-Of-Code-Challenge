afbc71f61a55a94c09b43adb1d200e1c738be84e
AREEBA NADEEM <AreebaNadeem567>
2024-08-12 15:53:31 +0500

    Day-03 commit

---
+++ Day-03/main.js
@@ -0,0 +1,14 @@
+/* Q7 - Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+// Addition
+console.log(5 + 3); //8
+// Subtraction
+console.log(10 - 2); //8
+// Multiplication
+console.log(2 * 4); //8
+// Division
+console.log(16 / 2); //8
+/* Q8 - You should create four lines that look like this: console.log(5 + 3) Your output should simply be four lines with the number 8 appearing once on each line.*/
+// (Refer to the answer for Question 7, as it serves the same purpose.)
+/* Q9 - Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.*/
+var favoriteNumber = 11;
+console.log("My favorite number is ".concat(favoriteNumber));
