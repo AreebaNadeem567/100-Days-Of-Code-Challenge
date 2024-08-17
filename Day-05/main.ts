2e100e3731f3c6ce3d705002aa18fc08c43367dc
Areeba Nadeem <159957660+AreebaNadeem567@users.noreply.github.com>
2024-08-17 13:12:47 +0500

    Update main.ts

--- Day-05/main.ts
+++ Day-05/main.ts
@@ -1,18 +1,10 @@
 //  Q13 - Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
-<<<<<<< HEAD
 let transportation: string[] = ["Honda Motorcycle", "Car", "Train"];
 
 for (let i = 0; i < transportation.length; i++) {
     console.log('I would like to own a ' + transportation[i]);
 };
-=======
- let transportation: string[] = ["Honda Motorcycle", "Car", "Train"];
-
- for (let i = 0; i < transportation.length; i++) {
-    console.log('I would like to own a ' + transportation[i]);
- };
->>>>>>> 6c12cf5ddb5fbe4f34f25244d2a8612097771e70
 
 // Q14 - Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
@@ -29,11 +21,8 @@ for (let index = 0; index < Guest_List.length; index++) {
 // Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
 
 // Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-<<<<<<< HEAD
-=======
 
 // Print a second set of invitation messages, one for each person who is still in your list.
->>>>>>> 6c12cf5ddb5fbe4f34f25244d2a8612097771e70
 
 // Print a second set of invitation messages, one for each person who is still in your list.
 
